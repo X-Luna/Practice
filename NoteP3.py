0bf563e6f151099433541614031c9269fbabc3e4
Luna <128177509+X-Luna@users.noreply.github.com>
2023-03-19 12:32:42 +0800

    Add files via upload

---
+++ NoteP3.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+
+
+#迭代
+#请使用迭代查找一个list中最小和最大值，并返回一个tuple：
+def findMinAndMax(L):
+    if L == []:
+        return (None, None)
+    else:
+        Min = L[0]
+        Max = L[0]
+        for num in L:
+            if num > Max:
+                Max = num
+            if num < Min:
+                Min = num
+    return (Min, Max)
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
+
+
+
+#列表生成式
+#请修改列表生成式，通过添加if语句保证列表生成式能正确地执行：
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+# 测试:
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+
+
+#生成器
+#把每一行看做一个list，试写一个generator，不断输出下一行的list：
+def triangles():
+    x = [1]
+    while True:
+        yield x
+        x = x + [0]
+        x = [x[i] + x[i-1] for i in range(len(x))]
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+n = 0
+results = []
+for t in triangles():
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+
+for t in results:
+    print(t)
+
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+
+
+#map/reduce
+#f利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+def normalize(name):
+    return name.capitalize()
+# 测试:
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+#Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+from functools import reduce
+def prod(L):
+    return reduce(lambda x, y: x * y, L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+from functools import reduce
+def str2float(s):
+    j = s.index('.')
+    def char2num(s):
+        digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+        return digits[s]
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s.replace('.', ''))) / (10 ** (len(s) - j - 1))
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+
+
+#filter
+#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()筛选出回数：
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+# 测试:
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+
+
+#sorted
+#假设我们用一组tuple表示学生名字和成绩：
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+#请用sorted()对上述列表分别按名字排序：
+def by_name(t):
+    return t[0]
+L2 = sorted(L, key=by_name)
+print(L2)
+
+#再按成绩从高到低排序：
+def by_score(t):
+    return -t[1]
+L2 = sorted(L, key=by_score)
+print(L2)
+
+
+
+#返回函数
+#利用闭包返回一个计数器函数，每次调用它返回递增整数：
+def createCounter():
+    i = [0]
+    def counter():
+        i[0] += 1
+        return i[0]
+    return counter
+# 测试:
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+
+
+#
